839c0e2b6ad39d91586fac78c6c48e7e0c49d210
HarshalPa <patilharshal110716@gmail.com>
2019-04-16 07:38:00 +0530

    ADDED NEW TES

---
+++ src/test/java/com/qa/test/HomePageTest.java
@@ -0,0 +1,18 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+	@Test
+		public void loginpage() {
+			System.out.println("Login Page");
+		}
+		
+	@Test
+		public void loginSet() {
+			System.out.println("Login Set");
+
+	}
+
+}
---
+++ src/test/java/com/qa/test/SearchPageTest.java
@@ -0,0 +1,17 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class SearchPageTest {
+	@Test
+	public void searchPageTest() {
+		System.out.println("Search Page Test1");
+	}
+	
+@Test
+	public void searchPageSet() {
+		System.out.println("Search Page Set Test1");
+
+}
+
+}
